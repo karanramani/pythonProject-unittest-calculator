14aa10c56291a1604dca4c81be0546750049d219
karanramani <kjr27@njit.edu>
2020-11-01 20:30:19 -0500

    Task: Setup proper docker image and tested addition file

---
+++ src/addition.py
@@ -0,0 +1,24 @@
+import unittest
+from Calculator import Calculator
+from CsvReader import CsvReader
+
+
+class MyAdditionTestCase(unittest.TestCase):
+    def setUp(self) -> None:
+        self.calculator = Calculator()
+
+    def test_instantiate_calculator(self):
+        self.assertIsInstance(self.calculator, Calculator)
+
+    def test_addition(self):
+        test_data = CsvReader('/src/Unit Test Addition.csv').data
+        for row in test_data:
+            self.assertEqual(self.calculator.add(row['Value 1'], row['Value 2']), float(row['Result']))
+            self.assertEqual(self.calculator.result, float(row['Result']))
+
+    def test_results_property(self):
+        self.assertEqual(self.calculator.result, 0)
+
+
+if __name__ == '__main__':
+    unittest.main()
